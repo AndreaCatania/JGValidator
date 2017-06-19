c840ea93566d0419bf20f9e602f3f028dac7df8b
Andrea Catania <info@andreacatania.com>
2017-06-19 15:12:58 +0200

    Added two API to add and remove elements

--- res/js/JGValidator.js
+++ res/js/JGValidator.js
@@ -1,5 +1,5 @@
 /**
- * Version 1.3.1
+ * Version 1.4
  * @param $elements
  * @param isLive
  * @constructor
@@ -8,296 +8,311 @@
  */
 var JGValidator = function( $elements, isLive ){
 
-    var self = this;
+	var self = this;
 
-    var animatedEff = 'animated flash';
-    var nodeOfErrorListElement = ".validator-error-list";
+	var animatedEff = 'animated flash';
+	var nodeOfErrorListElement = ".validator-error-list";
 
-    var regex_regex        = "data-validator-regex-regex";
-    var regex_message      = "data-validator-regex-message";
+	var regex_regex        = "data-validator-regex-regex";
+	var regex_message      = "data-validator-regex-message";
 
-    var required           = "data-validator-required";
-    var required_message   = "data-validator-required-message";
+	var required           = "data-validator-required";
+	var required_message   = "data-validator-required-message";
 
-    var min_length         = "data-validator-min-length";
-    var min_length_message = "data-validator-min-length-message";
+	var min_length         = "data-validator-min-length";
+	var min_length_message = "data-validator-min-length-message";
 
-    var max_length         = "data-validator-max-length";
-    var max_length_message = "data-validator-max-length-message";
+	var max_length         = "data-validator-max-length";
+	var max_length_message = "data-validator-max-length-message";
 
-    var ex_function         = "data-validator-function";
-    var ex_function_message = "data-validator-function-message";
+	var ex_function         = "data-validator-function";
+	var ex_function_message = "data-validator-function-message";
 
-    var   feedback_success = 'has-success'
-        , feedback_warning = 'has-warning'
-        , feedback_error = 'has-error';
+	var   feedback_success = 'has-success'
+		, feedback_warning = 'has-warning'
+		, feedback_error = 'has-error';
 
-    var firstInvalidElement = null;
+	var firstInvalidElement = null;
 
-    self.input_event    = "blur";
-    self.textarea_event = "blur";
-    self.select_event   = "change blur";
-    self.eventForClear  = "focus";
+	self.input_event    = "blur";
+	self.textarea_event = "blur";
+	self.select_event   = "change blur";
+	self.eventForClear  = "focus";
 
-    self.setInput_event = function( event ){
-        self.input_event = event;
-    };
+	self.setInput_event = function( event ){
+		self.input_event = event;
+	};
 
-    self.setTextarea_event = function( event ){
-        self.textarea_event = event;
-    };
+	self.setTextarea_event = function( event ){
+		self.textarea_event = event;
+	};
 
-    self.setSelect_event = function( event ){
-        self.select_event = event;
-    };
+	self.setSelect_event = function( event ){
+		self.select_event = event;
+	};
 
-    self.setEventForClear = function( event ){
-        self.eventForClear = event;
-    };
+	self.setEventForClear = function( event ){
+		self.eventForClear = event;
+	};
 
-    /**
-     * Metodo che ricarica i nuovi elementi da validare
-     */
-    self.replaceWith = function( $newElements ){
-        $elements.off("change input blur");
-        $elements = $newElements;
-        self.loadAllEventHand();
-    };
+	/**
+	 * This method replace all current inputs to check
+	 */
+	self.replaceWith = function( $newElements ){
+		$elements.off("change input blur");
+		$elements = $newElements;
+		self.loadAllEventHand();
+	};
 
-    self.getMessageAsListItem = function( $this, nameOfError ){
-        var errorMessage = $this.attr( nameOfError );
-        if( typeof( errorMessage )==="undefined" ){
-            return "";
-        }else{
-            return "<li>" + errorMessage + "</li>";
-        }
-    };
+	self.addElements = function( $elementsToAdd ){
+		$elementsToAdd.each(function(){
+			self.addElement($(this));
+		});
+	};
 
-    /**
-     * Checks if all fields are valid.
-     *
-     * @return true if all elements are valid otherwise false.
-     */
-    self.validate = function(animate){
-        var isValid = true;
-        firstInvalidElement = null;
-        $elements.each(function(){
-            var $this = $( this );
-            if( !validateElem( $this ) ){
+	self.addElement = function( $elementToAdd ){
+		$elements = $elements.add($elementToAdd);
+		self.loadEventHand($elementToAdd);
+	};
 
-                if(null==firstInvalidElement)
-                    firstInvalidElement = $this;
+	self.removeElements = function( $elementsToRemove ){
+		$elementsToRemove.off("change input blur");
+		$elements = $elements.not($elementsToRemove);
+	};
 
-                isValid = false;
-                if( typeof animate !== 'undefined' && animate )
-                    self.animate($this);
-            }
-        });
-        return isValid;
-    };
+	self.getMessageAsListItem = function( $this, nameOfError ){
+		var errorMessage = $this.attr( nameOfError );
+		if( typeof( errorMessage )==="undefined" ){
+			return "";
+		}else{
+			return "<li>" + errorMessage + "</li>";
+		}
+	};
 
-    self.getFirstInvalidElement = function(){
-        return firstInvalidElement;
-    };
+	/**
+	 * Checks if all fields are valid.
+	 *
+	 * @return true if all elements are valid otherwise false.
+	 */
+	self.validate = function(animate){
+		var isValid = true;
+		firstInvalidElement = null;
+		$elements.each(function(){
+			var $this = $( this );
+			if( !validateElem( $this ) ){
 
-    self.showTheseErrorsOnElem = function( errorsName, elemFilter ){
-        var $elem = $elements.filter( elemFilter );
-        showTheseErrorsOnElem( errorsName, $elem);
-    };
+				if(null==firstInvalidElement)
+					firstInvalidElement = $this;
 
-    self.showTheseWarningsOnElem = function( errorsName, elemFilter ){
-        var $elem = $elements.filter( elemFilter );
-        showTheseWarningsOnElem( errorsName, $elem);
-    };
+				isValid = false;
+				if( typeof animate !== 'undefined' && animate )
+					self.animate($this);
+			}
+		});
+		return isValid;
+	};
 
-    self.showSuccessOnElem = function( elemFilter ){
-        showSuccessOnElem( $elements.filter( elemFilter ) );
-    };
+	self.getFirstInvalidElement = function(){
+		return firstInvalidElement;
+	};
 
-    self.hideAllFeedback = function(){
-        $elements.each(function(){
-            hideAllFeedbackToElement( $(this) );
-        });
-    };
+	self.showTheseErrorsOnElem = function( errorsName, elemFilter ){
+		var $elem = $elements.filter( elemFilter );
+		showTheseErrorsOnElem( errorsName, $elem);
+	};
 
-    self.hideAllFeedbackToElement = function( elemFilter ){
-        hideAllFeedbackToElement( $elements.filter( elemFilter ) );
-    };
+	self.showTheseWarningsOnElem = function( errorsName, elemFilter ){
+		var $elem = $elements.filter( elemFilter );
+		showTheseWarningsOnElem( errorsName, $elem);
+	};
 
-    /**
-     * If the element isn't animable (not have animated class) set true to add this class.
-     * @param $elem
-     */
-    self.animate = function( $elem ){
-        // Using animate.css library this snippets provides an animation
-        // for all elements that aren't valid and have the class "animated"
-        $elem.addClass( animatedEff )
-            .one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
-                $elem.removeClass( animatedEff )
-            });
-    };
+	self.showSuccessOnElem = function( elemFilter ){
+		showSuccessOnElem( $elements.filter( elemFilter ) );
+	};
 
-    /**
-     * Se impostato su live la verifica del contenuto degli elementi viene fato immediatamente
-     */
-    self.loadAllEventHand = function(){
-        if( isLive ){                                                                        // se live appendo per ogni elemento un controllo che valida l'elemento
-            $elements.each(function(){
+	self.hideAllFeedback = function(){
+		$elements.each(function(){
+			hideAllFeedbackToElement( $(this) );
+		});
+	};
 
-                var $this = $( this );
-                var event = "";
+	self.hideAllFeedbackToElement = function( elemFilter ){
+		hideAllFeedbackToElement( $elements.filter( elemFilter ) );
+	};
 
-                if( $this.is("input") ){
-                    event = self.input_event;
-                }else
-                if( $this.is("select") ){
-                    event = self.select_event;
-                }else
-                if( $this.is("textarea") ){
-                    event = self.textarea_event;
-                }
+	/**
+	 * If the element isn't animable (not have animated class) set true to add this class.
+	 * @param $elem
+	 */
+	self.animate = function( $elem ){
+		// Using animate.css library this snippets provides an animation
+		// for all elements that aren't valid and have the class "animated"
+		$elem.addClass( animatedEff )
+			.one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
+				$elem.removeClass( animatedEff )
+			});
+	};
 
-                /**
-                 * Script usato per validare gli elementi al
-                 * verificardi di ben precisi eventi
-                 */
-                $this.on( event, function() {
-                    validateElem( $( this ) );
-                });
+	/**
+	 * If isLive is true the inputs are checked in real time
+	 */
+	self.loadAllEventHand = function(){
+		if( isLive ){
+			self.loadEventHand($( this ));
+		}
+	};
 
-                /**
-                 * Questo script pulisce l'elemento quando si verifica l'evento contenuto in "eventForClear"
-                 *
-                 * Di default è impostato con focus, cioè quando un utente entra nel
-                 * campo tutti i messaggi vengono nascosti cosi da non confondere l'utente
-                 */
-                $this.on( self.eventForClear, function() {
-                    self.hideAllFeedbackToElement( $( this ) );
-                });
+	self.loadEventHand = function($inp){
+		if( isLive ){
+			var event = "";
 
-            });
+			if( $inp.is("input") ){
+				event = self.input_event;
+			}else
+			if( $inp.is("select") ){
+				event = self.select_event;
+			}else
+			if( $inp.is("textarea") ){
+				event = self.textarea_event;
+			}
 
-        }
+			/**
+			 * Script usato per validare gli elementi al
+			 * verificardi di ben precisi eventi
+			 */
+			$inp.on( event, function() {
+				validateElem( $( this ) );
+			});
 
-    };
+			/**
+			 * Questo script pulisce l'elemento quando si verifica l'evento contenuto in "eventForClear"
+			 *
+			 * Di default è impostato con focus, cioè quando un utente entra nel
+			 * campo tutti i messaggi vengono nascosti cosi da non confondere l'utente
+			 */
+			$inp.on( self.eventForClear, function() {
+				self.hideAllFeedbackToElement( $( this ) );
+			});
+		}
+	};
 
-    var validateElem = function( $this ){
-        var   isValid = true
-            , errors = []
-            , value = $this.val();
-                
-        if(null!=value){
-            value = $.trim(value);
-        }
+	var validateElem = function( $this ){
+		var   isValid = true
+			, errors = []
+			, value = $this.val();
 
-        // Error checking
-        if( typeof( $this.attr( required ) )!=="undefined" ){
-            if( null==value || ""==value ){
-                isValid = false;
-                errors.push(required_message);
-            }
-        }
+		if(null!=value){
+			value = $.trim(value);
+		}
 
-        // Regex processed only if the value isn't void
-        if( null!=value && ""!=value ){
-            var inputTxtRegex = $this.attr( regex_regex );
-            if( typeof( inputTxtRegex )!=="undefined" ){
-                var regex = new RegExp( inputTxtRegex );
-                if( !regex.test( value ) ){
-                    isValid = false;
-                    errors.push(regex_message);
-                }
-            }
-        }
+		// Error checking
+		if( typeof( $this.attr( required ) )!=="undefined" ){
+			if( null==value || ""==value){
+				isValid = false;
+				errors.push(required_message);
+			}
+		}
 
-        var attrMin_length = $this.attr( min_length );
-        if( typeof( attrMin_length )!=="undefined" ){
-            var nMin_length = parseInt( attrMin_length );
-            if( nMin_length > value.length ){
-                isValid = false;
-                errors.push(min_length_message);
-            }
-        }
+		// Regex processed only if the value isn't void
+		if( null!=value && ""!=value ){
+			var inputTxtRegex = $this.attr( regex_regex );
+			if( typeof( inputTxtRegex )!=="undefined" ){
+				var regex = new RegExp( inputTxtRegex );
+				if( !regex.test( value ) ){
+					isValid = false;
+					errors.push(regex_message);
+				}
+			}
+		}
 
-        var attrMax_length = $this.attr( max_length );
-        if( typeof( attrMax_length )!=="undefined" ){
-            var nMax_length = parseInt( attrMax_length );
-            if( nMax_length < value.length ){
-                isValid = false;
-                errors.push(max_length_message);
-            }
-        }
+		var attrMin_length = $this.attr( min_length );
+		if( typeof( attrMin_length )!=="undefined" ){
+			var nMin_length = parseInt( attrMin_length );
+			if( nMin_length > value.length ){
+				isValid = false;
+				errors.push(min_length_message);
+			}
+		}
 
-        var attrEx_function = $this.attr( ex_function );
-        if( typeof( attrEx_function )!=="undefined" ){
-            if( !window[attrEx_function]( $this, self ) ){
-                isValid = false;
-                errors.push(ex_function_message);
-            }
-        }
+		var attrMax_length = $this.attr( max_length );
+		if( typeof( attrMax_length )!=="undefined" ){
+			var nMax_length = parseInt( attrMax_length );
+			if( nMax_length < value.length ){
+				isValid = false;
+				errors.push(max_length_message);
+			}
+		}
 
-        if( !isValid ){
-            showTheseErrorsOnElem(errors, $this);
-        }else{
-            self.showSuccessOnElem($this);
-        }
+		var attrEx_function = $this.attr( ex_function );
+		if( typeof( attrEx_function )!=="undefined" ){
+			if( !window[attrEx_function]( $this, self ) ){
+				isValid = false;
+				errors.push(ex_function_message);
+			}
+		}
 
-        return isValid;
-    };
+		if( !isValid ){
+			showTheseErrorsOnElem(errors, $this);
+		}else{
+			self.showSuccessOnElem($this);
+		}
 
-    var showTheseErrorsOnElem = function( errorsName, $elem ){
+		return isValid;
+	};
 
-        var $jgvalidInputGroup = $elem.closest(".jgvalid-input-group");
-        $jgvalidInputGroup.find(".fieldErrors").remove();
+	var showTheseErrorsOnElem = function( errorsName, $elem ){
 
-        var errorLItems = getMessageListItems($elem, errorsName);
+		var $jgvalidInputGroup = $elem.closest(".jgvalid-input-group");
+		$jgvalidInputGroup.find(".fieldErrors").remove();
 
-        if( ''!=errorLItems ){
-            setMessageListItems($elem, $jgvalidInputGroup, errorLItems);
-        }
+		var errorLItems = getMessageListItems($elem, errorsName);
 
-        $jgvalidInputGroup.addClass(feedback_error).removeClass(feedback_success + " " + feedback_warning);
-    };
+		if( ''!=errorLItems ){
+			setMessageListItems($elem, $jgvalidInputGroup, errorLItems);
+		}
 
-    var showTheseWarningsOnElem = function( errorsName, $elem ){
+		$jgvalidInputGroup.addClass(feedback_error).removeClass(feedback_success + " " + feedback_warning);
+	};
 
-        var $jgvalidInputGroup = $elem.closest(".jgvalid-input-group");
-        $jgvalidInputGroup.find(".fieldErrors").remove();
+	var showTheseWarningsOnElem = function( errorsName, $elem ){
 
-        var errorLItems = getMessageListItems($elem, errorsName);
+		var $jgvalidInputGroup = $elem.closest(".jgvalid-input-group");
+		$jgvalidInputGroup.find(".fieldErrors").remove();
 
-        if( ''!=errorLItems ){
-            setMessageListItems($elem, $jgvalidInputGroup, errorLItems);
-        }
-        $jgvalidInputGroup.addClass(feedback_warning).removeClass(feedback_success + " " + feedback_error);
-    };
-    var showSuccessOnElem = function( $elem ){
-        var $jgvalidInputGroup = $elem.closest(".jgvalid-input-group");
-        $jgvalidInputGroup.find(".fieldErrors").remove();
-        $jgvalidInputGroup.addClass(feedback_success).removeClass(feedback_warning + " " + feedback_error);
-    };
+		var errorLItems = getMessageListItems($elem, errorsName);
 
-    var getMessageListItems = function( $elem, errorsName ){
-        var errorLItems = '';
-        $.each( errorsName, function( index, value ){
-            errorLItems += self.getMessageAsListItem( $elem, value );
-        });
-        return errorLItems;
-    };
+		if( ''!=errorLItems ){
+			setMessageListItems($elem, $jgvalidInputGroup, errorLItems);
+		}
+		$jgvalidInputGroup.addClass(feedback_warning).removeClass(feedback_success + " " + feedback_error);
+	};
+	var showSuccessOnElem = function( $elem ){
+		var $jgvalidInputGroup = $elem.closest(".jgvalid-input-group");
+		$jgvalidInputGroup.find(".fieldErrors").remove();
+		$jgvalidInputGroup.addClass(feedback_success).removeClass(feedback_warning + " " + feedback_error);
+	};
 
-    var setMessageListItems = function( $elem, $jgvalidInputGroup, errorLItems ){
-        var $putHereErrors = $jgvalidInputGroup.find( nodeOfErrorListElement );
-        if( 0>=$putHereErrors.length ){
-            $putHereErrors = $elem.closest("div");
-        }
-        $putHereErrors.append("<ul class='fieldErrors'>"+errorLItems+"</ul>");
-    };
+	var getMessageListItems = function( $elem, errorsName ){
+		var errorLItems = '';
+		$.each( errorsName, function( index, value ){
+			errorLItems += self.getMessageAsListItem( $elem, value );
+		});
+		return errorLItems;
+	};
 
-    var hideAllFeedbackToElement = function( $elem ){
-        var $jgvalidInputGroup = $elem.closest(".jgvalid-input-group");
-        $jgvalidInputGroup.removeClass( feedback_error + " " + feedback_success + " " + feedback_warning );
-        $jgvalidInputGroup.find(".fieldErrors").remove();
-    };
+	var setMessageListItems = function( $elem, $jgvalidInputGroup, errorLItems ){
+		var $putHereErrors = $jgvalidInputGroup.find( nodeOfErrorListElement );
+		if( 0>=$putHereErrors.length ){
+			$putHereErrors = $elem.closest("div");
+		}
+		$putHereErrors.append("<ul class='fieldErrors'>"+errorLItems+"</ul>");
+	};
 
-    self.loadAllEventHand();
+	var hideAllFeedbackToElement = function( $elem ){
+		var $jgvalidInputGroup = $elem.closest(".jgvalid-input-group");
+		$jgvalidInputGroup.removeClass( feedback_error + " " + feedback_success + " " + feedback_warning );
+		$jgvalidInputGroup.find(".fieldErrors").remove();
+	};
+
+	self.loadAllEventHand();
 };
